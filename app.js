001abcd3acf015a79e49d4bbb7d39a4405cdbf42
unknown <immanuelderrin008@gmail.com>
2024-03-07 11:15:49 +0530

    Worked on Javascript

--- app.js
+++ app.js
@@ -1,99 +1,28 @@
 var cartValue = document.getElementById("cart-value");
 var cartButton = document.getElementById("cart");
-
 var addButtons = document.getElementsByClassName("button");
 
 var items = [
-  {
-    name: "This was our pact",
-    quantity: 0,
-    dollars: 7,
-    cents: 49,
-  },
-  {
-    name: "The famous five",
-    quantity: 0,
-    dollars: 4,
-    cents: 59,
-  },
-  {
-    name: "Matilda",
-    quantity: 0,
-    dollars: 6,
-    cents: 80,
-  },
-  {
-    name: "Harry Potter",
-    quantity: 0,
-    dollars: 10,
-    cents: 0,
-  },
-  {
-    name: "For Young Readers",
-    quantity: 0,
-    dollars: 7,
-    cents: 29,
-  },
-  {
-    name: "Wimpy Kid - DIY",
-    quantity: 0,
-    dollars: 4,
-    cents: 99,
-  },
-  {
-    name: "Dart Board",
-    quantity: 0,
-    dollars: 17,
-    cents: 49,
-  },
-  {
-    name: "Connect Four",
-    quantity: 0,
-    dollars: 19,
-    cents: 99,
-  },
-  {
-    name: "Jenga",
-    quantity: 0,
-    dollars: 20,
-    cents: 99,
-  },
-  {
-    name: "Monopoly",
-    quantity: 0,
-    dollars: 19,
-    cents: 49,
-  },
-  {
-    name: "Bookmarks",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Birthday Card",
-    quantity: 0,
-    dollars: 12,
-    cents: 49,
-  },
-  {
-    name: "Stuffed toys",
-    quantity: 0,
-    dollars: 15,
-    cents: 99,
-  },
-  {
-    name: "Dream catcher drawing",
-    quantity: 0,
-    dollars: 18,
-    cents: 49,
-  },
+  { name: "This was our pact", quantity: 0, dollars: 7, cents: 49 },
+  { name: "The famous five", quantity: 0, dollars: 4, cents: 59 },
+  { name: "Matilda", quantity: 0, dollars: 6, cents: 80 },
+  { name: "Harry Potter", quantity: 0, dollars: 10, cents: 0 },
+  { name: "For Young Readers", quantity: 0, dollars: 7, cents: 29 },
+  { name: "Wimpy Kid - DIY", quantity: 0, dollars: 4, cents: 99 },
+  { name: "Dart Board", quantity: 0, dollars: 17, cents: 49 },
+  { name: "Connect Four", quantity: 0, dollars: 19, cents: 99 },
+  { name: "Jenga", quantity: 0, dollars: 20, cents: 99 },
+  { name: "Monopoly", quantity: 0, dollars: 19, cents: 49 },
+  { name: "Bookmarks", quantity: 0, dollars: 12, cents: 49 },
+  { name: "Birthday Card", quantity: 0, dollars: 12, cents: 49 },
+  { name: "Stuffed toys", quantity: 0, dollars: 15, cents: 99 },
+  { name: "Dream catcher drawing", quantity: 0, dollars: 18, cents: 49 },
 ];
 
 function updateCart() {
   let cart = 0;
-  for (index = 0; index < items.length; index++) {
-    cart = cart + items[index].quantity;
+  for (let index = 0; index < items.length; index++) {
+    cart += items[index].quantity;
   }
   cartValue.innerHTML = cart;
 }
@@ -111,32 +40,38 @@ var finalCents = 0;
 function updatePrice() {
   let totalPriceInCents = 0;
 
-  for (index = 0; index < items.length; index++) {
-    totalPriceInCents =
-      totalPriceInCents +
+  for (let index = 0; index < items.length; index++) {
+    totalPriceInCents +=
       items[index].quantity * (items[index].dollars * 100 + items[index].cents);
   }
+
   finalDollars = Math.floor(totalPriceInCents / 100);
   finalCents = totalPriceInCents % 100;
 }
 
 
-cartButton.onclick = () => {
-  updatePrice();
-
+function formatOrderDetails() {
+  let orderMessage = "Order Details:\n";
 
   for (let index = 0; index < items.length; index++) {
-    if (items[index].quantity != 0) {
-      console.log(
-        "Item name: " +
-          items[index].name +
-          " - Quantity: " +
-          items[index].quantity
-      );
+    if (items[index].quantity !== 0) {
+      orderMessage += ${items[index].name} - Quantity: ${items[index].quantity}\n;
     }
   }
 
-  console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
-  );
+  orderMessage += \nTotal Amount: ${finalDollars}.${finalCents} dollars;
+
+  return orderMessage;
+}
+
+cartButton.onclick = () => {
+  updatePrice();
+
+  const orderDetails = formatOrderDetails();
+  
+  const recipientNumber = '9344327004';
+  
+  const whatsappLink = https://wa.me/${recipientNumber}?text=${encodeURIComponent(orderDetails)};
+
+  window.open(whatsappLink, '_blank');
 };
